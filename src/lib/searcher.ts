8ce1b601ce5da0e420cc76738fe11e1f1260cbd6
zhuyonglong <110017640+hxnzyl@users.noreply.github.com>
2024-11-27 19:27:34 +0800

    Searcher

--- src/lib/searcher.ts
+++ src/lib/searcher.ts
@@ -2,6 +2,13 @@ import { SearcherProps } from '@/components/searcher'
 import { escape } from 'lodash-es'
 
 const SearcherUtils = {
+	/**
+	 * Return RegExp based on keyword and SearcherProps
+	 *
+	 * @param keyword
+	 * @param props
+	 * @returns
+	 */
 	regexp(keyword: string, props: SearcherProps): RegExp | string {
 		// Case Sensitive
 		const caseIn = props.matchCase ? undefined : 'i'
@@ -14,6 +21,16 @@ const SearcherUtils = {
 		} catch (e) {}
 		return keyword
 	},
+
+	/**
+	 * Filter items based on keyword and SearcherProps
+	 *
+	 * @param items
+	 * @param fields
+	 * @param keyword
+	 * @param props
+	 * @returns
+	 */
 	filter<T>(items: T[], fields: (keyof T)[], keyword: string, props: SearcherProps) {
 		if (keyword != null && keyword !== '') {
 			const regexp = SearcherUtils.regexp(keyword, props)
